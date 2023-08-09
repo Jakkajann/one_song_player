e21bb2909c045bd830bd9cd8d213af38dd5cd3e2
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-08-09 17:18:46 -0300

    Finalize player one song

--- scripts/index.js
+++ scripts/index.js
@@ -5,16 +5,35 @@ let currentPlaying = {
     song: null
 }
 
+const progress = $("#musicProgress");
+
+let interval = null;
+
+function updateBar() {
+    $(progress).css("width", currentPlaying.audio.currentTime);
+}
+
+
 function playSong(element) {
     const song = $(element).attr("song");
     var audio = new Audio(song);
     audio.volume = 0.4;
+    const duration = audio.duration;
+    $(".progress").attr("aria-value-max", audio.duration);
 
     if (song === currentPlaying.song && isPlaying) {
         currentPlaying.audio.pause();
+        clearInterval(interval);
+        $(element).text("Play");
+        $(element).removeClass("btn-danger");
+        $(element).addClass("btn-success");
         isPlaying = !isPlaying;
     } else if(song === currentPlaying.song && !isPlaying){
+        $(element).removeClass("btn-success");
+        $(element).addClass("btn-danger");
+        $(element).text("Pause")
         currentPlaying.audio.play();
+        interval = setInterval(updateBar, 1000);
         isPlaying = !isPlaying;
     } else {
         if(currentPlaying.audio)
@@ -22,8 +41,12 @@ function playSong(element) {
         audio.play();
         currentPlaying.audio = audio;
         currentPlaying.audio.play();
+        $(element).removeClass("btn-success");
+        $(element).addClass("btn-danger");
+        $(element).text("Pause")
         currentPlaying.button = element;
         currentPlaying.song = song;
         isPlaying = !isPlaying;
+        interval = setInterval(updateBar, 1000);
      }
 }
